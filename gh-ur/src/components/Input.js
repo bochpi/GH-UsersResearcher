e6102efcfdbb26bf420272e34060b15b5010f46b
Iteron901 <bochra.piotr@gmail.com>
2018-01-31 01:39:59 +0100

    split users in input

--- gh-ur/src/components/Input.js
+++ gh-ur/src/components/Input.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 
 class Input extends Component {
   
-    render() {
-      return (
-        <span>
-          <input type="text" placeholder="Put GitHub Users..." />
-          <button>OK</button>
-        </span>
-      );
-    }
+  state = {currentInputContent: '', usersList: []};
+
+  handleInputChange = (e) => {
+    e.preventDefault();
+    const splittedUsers = e.target.value.split(" ");
+    this.setState({currentInputContent: e.target.value, usersList: splittedUsers});
   }
-  
-  export default Input;
+
+  render() {
+    console.log({inputValue: this.state.usersList});
+    return (
+      <span>
+        <input type="text" value={this.state.currentInputContent} placeholder="Put GitHub Users..."
+        onChange={this.handleInputChange} />
+        <button>OK</button>
+      </span>
+    );
+  }
+}
+
+export default Input;
   
